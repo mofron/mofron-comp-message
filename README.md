e3730cf0e28ff7c8e0b7c3aea9afdb9306c29931
root <root@ip-172-31-10-115.ap-northeast-1.compute.internal>
2019-06-30 01:19:06 +0000

    mod comment

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # mofron-comp-message
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
 message component for mofron
+
+it is a component for informing the user of the result and notification contents.
+
+## Feature
+ - display time can configure by "timer" parameter.
+ - display position is easy to configure by "fixpos" parameter.
+ - when the display position is set by "fixpos", the display position is fixed even if you scroll.
+## Attention
+ - default visible is false. it needs to be displayed by the javascript "visible" function
+
+# Install
+```
+npm install mofron mofron-comp-message
+```
+
+# Sample
+```html
+<require>
+    <tag module="mofron-comp-message">Message</tag>
+</require>
+
+<script>
+msg.visible(true);
+</script>
+
+<Message name=msg fixpos="right","bottom" timer=3>
+    message
+</Message>
+```
+# Parameter
+
+|Simple<br>Param | Parameter Name | Type | Description |
+|:--------------:|:---------------|:-----|:------------|
+| | fixpos | string ["left"/"center"/"right"] | horizonal position |
+| | | string ["top"/"center"/"bottom"] | vertical position |
+| | x_fixpos | string ["left"/"center"/"right"] | horizonal fixed position |
+| | y_fixpos | string ["top"/"center"/"bottom"] | vertical fixed position |
+| | offset | string (size) | position offset, default is "0.5rem" |
+| | closeVisible | boolean | true: visible close component <default> |
+| | | | false: invisible close component  |
+| | closeComp | component | replacement close component |
+| | timer | number | display message timer |
+
--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 /**
- * @file   mofron-comp-message/index.js
- * @brief  message component for mofron
+ * @file mofron-comp-message/index.js
+ * @brief message component for mofron
+ *        it is a component for informing the user of the result and notification contents.
+ * @feature display time can configure by "timer" parameter.
+ *          display position is easy to configure by "fixpos" parameter.
+ *          when the display position is set by "fixpos", the display position is fixed even if you scroll.
+ * @attention default visible is false. it needs to be displayed by the javascript "visible" function
  * @author simpart
  */
-const mf      = require('mofron');
-const Frame   = require('mofron-comp-frame');
-const Text    = require('mofron-comp-text');
-const Horiz   = require('mofron-layout-horizon');
-const VrtPos  = require('mofron-effect-vrtpos');
-const SyncHei = require('mofron-effect-synchei');
+const mf     = require("mofron");
+const Frame  = require("mofron-comp-txtframe");
+const Text   = require("mofron-comp-text");
+const Close  = require("mofron-comp-close");
+const Vrtpos = require("mofron-effect-vrtpos");
+const Hrzpos = require("mofron-effect-hrzpos");
 
-mf.comp.Message = class extends mf.Component {
+mf.comp.Message = class extends Frame {
     /**
      * initialize message component
      *
-     * @param p1 (object) component option
-     * @param p1 (string) message
+     * @param (mixed) string/mofron-comp-text: message text contents
+     *                object: component option
+     * @pmap text
+     * @type private
      */
     constructor (po) {
         try {
             super();
-            this.name('Message');
-            this.prmMap('text');
+            this.name("Message");
+            this.prmMap("text");
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -32,15 +39,25 @@ mf.comp.Message = class extends mf.Component {
     /**
      * initialize dom contents
      * 
-     * @note private method
+     * @type private
      */
     initDomConts () {
         try {
+            /* dom */
             super.initDomConts();
-            this.addChild(this.frame());
-            this.target(this.frame().target());
-            this.addChild(this.text());
-            this.size('3.5rem', '0.5rem');
+            this.child([this.closeComp()]);
+            
+            /* effect */
+            this.effect([
+                new Hrzpos({ suspend:true }),
+                new Vrtpos({ suspend:true })
+            ]);
+            
+            /* style */
+            this.x_center(false);
+            this.style({ "display":"flex" });
+            this.size("3.5rem", "0.5rem");
+            this.visible(false);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -48,16 +65,40 @@ mf.comp.Message = class extends mf.Component {
     }
     
     /**
-     * frame component setter/getter
-     *
-     * @param (Frame) frame component
-     * @return (Frame) frame component
+     * fixed position config
+     * 
+     * @param (string ["left"/"center"/"right"]) horizonal position
+     * @param (string ["top"/"center"/"bottom"]) vertical position
+     * @type parameter
      */
-    frame (prm) {
+    fixpos (xpos, ypos) {
         try {
-            let ret = this.innerComp('frame', prm, Frame);
+            if (undefined === xpos) {
+                return [this.member("x_fixpos"), this.member("y_fixpos")];
+            }
+            this.x_fixpos(xpos);
+            this.y_fixpos(ypos);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * horizonal fixed position
+     * 
+     * @param (string ["left"/"center"/"right"]) horizonal fixed position
+     * @return (string) horizonal fixed position
+     * @type parameter
+     */
+    x_fixpos (prm) {
+        try {
+            let ret = this.member("x_fixpos", ["left","center","right"], prm);
             if (undefined !== prm) {
-                prm.option({ layout : new Horiz() });
+                this.style({"position" : "fixed"});
+                this.effect(
+                    new Hrzpos(prm, ("center" !== prm) ? this.offset() : undefined)
+                );
             }
             return ret;
         } catch (e) {
@@ -67,32 +108,112 @@ mf.comp.Message = class extends mf.Component {
     }
     
     /**
-     * message text setter/getter
-     *
-     * @param (string) message text
-     * @param (Text) message text component
-     * @return (Text) message text component
+     * vertical fixed position
+     * 
+     * @param (string ["top"/"center"/"bottom"]) vertical fixed position
+     * @return (string) vertical fixed position
+     * @type parameter
      */
-    text (prm) {
+    y_fixpos (prm) {
         try {
-            if ('string' === typeof prm) {
-                this.text().option({ text : prm });
-                return;
-            }
-            
-            let ret = this.innerComp('text', prm, Text);
+            let ret = this.member("y_fixpos", ["top","center","bottom"], prm); 
             if (undefined !== prm) {
+                this.style({"position" : "fixed"});
+                this.effect(
+                    new Vrtpos(prm, ("center" !== prm) ? this.offset() : undefined)
+                );
+            }
+            return ret;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * position offset
+     * 
+     * @param (string (size)) position offset, default is "0.5rem"
+     * @return (string (size)) position offset
+     * @type parameter
+     */
+    offset (prm) {
+        try {
+            if (undefined !== prm) {
+                prm = mf.func.getSize(prm).toString();
+            }
+            return this.member("offset", "string", prm, "0.5rem");
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * close component visible
+     * 
+     * @param (boolean) true: visible close component <default>
+     *                  false: invisible close component 
+     * @type parameter
+     */
+    closeVisible (prm) {
+        try { return this.innerComp("close").visible(prm); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * close component
+     *
+     * @param (component) replacement close component
+     * @type parameter
+     */
+    closeComp (prm) {
+        try {
+            if (true === mf.func.isComp(prm)) {
                 prm.option({
-                    sizeValue : [ 'margin-left', '0.2rem' ],
-                    effect    : [
-                        new VrtPos('center'),
-                        new SyncHei({
-                            targetComp: this, offset: '-0.3rem', tag: this.name() + '-text'
-                        })
-                    ]
+                    size: "0.23rem",
+                    style: [{ "position":"relative" },{locked:true}]
                 });
             }
-            return ret;
+            return this.innerComp("closeComp", prm, Close);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    } 
+    
+    /**
+     * display timer
+     * 
+     * @param (number) display message timer
+     * @return (number) display message timer
+     * @type parameter
+     */
+    timer (prm) {
+        try { return this.member("timer", "number", prm, 0); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * message width
+     * set close position
+     *
+     * @param (string (size)) message width
+     * @type private
+     */
+    width (prm) {
+        try {
+            if (undefined !== prm) {
+                let wid = mf.func.getSize(prm);
+                this.closeComp().style({
+                    "left": ("%" === wid.type()) ? mf.func.sizeSum(wid, "-10%") : (wid.value() - 0.3) + wid.type()
+                });
+            }
+            return super.width(prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -100,42 +221,30 @@ mf.comp.Message = class extends mf.Component {
     }
     
     /**
-     * text color setter/getter
-     * 
-     * @param (string) text color (css value)
-     * @param (Array) text color ([red(0-255), green(0-255), blue(0-255)])
-     * @return (string) text color
+     * @type private
      */
-    mainColor (prm) {
-        try { return this.text().color(prm); } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    /**
-     * frame background color setter/getter
-     *
-     * @param (string) frame background color (css value)
-     * @param (Array) frame background color ([red(0-255), green(0-255), blue(0-255)])
-     * @return (string) frame background color
-     */
-    baseColor (clr) {
-        try { return this.frame().mainColor(clr); } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    /**
-     * frame border color setter/getter
-     *
-     * @param (string) frame border color (css value)
-     * @param (Array) frame border color ([red(0-255), green(0-255), blue(0-255)])
-     * @return (string) frame border color
-     */
-    accentColor (prm) {
-        try { return this.frame().accentColor(prm); } catch (e) {
+    visible (flg, cb) {
+        try {
+            let msg = this;
+            if (true === flg) {
+                let msg_cb = () => {
+                    try {
+                        if ((true === flg) && (0 !== msg.timer())) {
+                            setTimeout(() => { msg.visible(false); }, msg.timer()*1000);
+                        }
+                        if ("function" === typeof cb) {
+                            cb();
+                        }
+                    } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+                    }
+                }
+                super.visible(flg, msg_cb);
+                return;
+            }
+            return super.visible(flg);
+        } catch (e) {
             console.error(e.stack);
             throw e;
         }
